d83cf0540b959399933474cdff1e6bebcba08bae
Bar Pisahov <barpesahov@gmail.com>
2022-02-06 20:41:29 +0200

    fix no key error

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -4,13 +4,13 @@ import './Loader.scss';
 
 function Loader() {
     return (
-        <div class="ad">
-            {[...Array(6)].map(() => (
-                <section>
-                    <img class="header-img skeleton" alt="" />
-                    <div class="skeleton skeleton-text"></div>
-                    <div class="skeleton skeleton-text"></div>
-                    <div class="skeleton skeleton-text"></div>
+        <div className="ad">
+            {[...Array(6)].map((_obj, idx) => (
+                <section key={idx}>
+                    <img className="header-img skeleton" alt="" />
+                    <div className="skeleton skeleton-text"></div>
+                    <div className="skeleton skeleton-text"></div>
+                    <div className="skeleton skeleton-text"></div>
                 </section>
             ))}
         </div>
